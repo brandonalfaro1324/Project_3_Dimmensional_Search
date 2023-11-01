3411f4c30291b30e4c14151e14d0cf0b82bfcbe2
brandonalfaro1324 <alfarobrandon25@gmail.com>
2023-11-01 18:21:26 -0500

    Added an Entry class that hold id, cost, and description

--- MDS.java
+++ MDS.java
@@ -6,15 +6,48 @@
 package bxa220020;
 
 import java.util.HashMap;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import java.util.Iterator;
+
+import java.util.List;
 
 
 // If you want to create additional classes, place them in this file as subclasses of MDS
 
 public class MDS {
-    // Add fields of MDS here
 
+    // Entry class that holds id, cost, and TreeSet that holds description
+    static class Entry{
+        int id = -1;
+        int cost = -1;
+        TreeSet<Integer> TSet = null;  
+
+        // Entry Constructor
+        Entry(int id, int cost, List<Integer> new_list){
+            this.id = id;
+            this.cost = cost;   
+            this.TSet = new TreeSet<>();
+
+            // For TreeSet, add every description from List
+            for(int element : new_list){
+                TSet.add(element);
+            }
+        }
+    }
+
+
+    //TreeMap<Integer, Entry> TreeMap = null;
+    //HashMap<Integer, TreeSet<Integer>> hash_map = null;
+
+
+
+
+    // Add fields of MDS here
     // Constructors
     public MDS() {
+       //hash_map = new HashMap<>(null);
     }
 
     /* Public methods of MDS. Do not change their signatures.
@@ -26,14 +59,21 @@ public class MDS {
        Returns 1 if the item is new, and 0 otherwise.
     */
     public int insert(int id, int price, java.util.List<Integer> list) {
-	return 0;
+
+        /*      <TESTING ENTRY>
+        Entry new_entry = new Entry(id, price, list);
+        Iterator<Integer> testing = new_entry.TSet.iterator();   
+        while(testing.hasNext()){
+            Integer element = testing.next();
+            System.out.println(element);  
+        }
+         */
+        
+        return 0;
     }
 
     // b. Find(id): return price of item with given id (or 0, if not found).
     public int find(int id) {
-
-
-
 	    return 0;
     }
 
