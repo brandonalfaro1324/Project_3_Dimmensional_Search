3db40569d3f7f9f8b2cee4c0b4aa5578cbd433f4
brandonalfaro1324 <alfarobrandon25@gmail.com>
2023-11-01 21:17:15 -0500

    Added a working insert() function, also added  print_result() to better see Keys and Values

--- MDS.java
+++ MDS.java
@@ -9,6 +9,9 @@ import java.util.HashMap;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+import javax.swing.RowFilter.Entry;
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
 import java.util.Iterator;
 
 import java.util.List;
@@ -18,37 +21,57 @@ import java.util.List;
 
 public class MDS {
 
-    // Entry class that holds id, cost, and TreeSet that holds description
-    static class Entry{
+    // Item class that holds id, cost, and TreeSet that holds description
+    static class Item{
         int id = -1;
         int cost = -1;
-        TreeSet<Integer> TSet = null;  
+        TreeSet<Integer> TSetDescr = null;  
 
         // Entry Constructor
-        Entry(int id, int cost, List<Integer> new_list){
+        Item(int id, int cost, List<Integer> new_list){
             this.id = id;
             this.cost = cost;   
-            this.TSet = new TreeSet<>();
+            this.TSetDescr = new TreeSet<>();
 
             // For TreeSet, add every description from List
             for(int element : new_list){
-                TSet.add(element);
+                this.TSetDescr.add(element);
             }
+        }
+
+        // Setting new Price
+        private void setprice(int new_price){
+            this.cost = new_price;
+        }
+
+        // Setting new List by setting variable to null then allocating a new one
+        private void setlist(List<Integer> new_list){
+            this.TSetDescr = null;
+            this.TSetDescr = new TreeSet<>();
+
+            // For TreeSet, add every description from List
+            for(int element : new_list){
+                this.TSetDescr.add(element);
+            }
+        }
+
+        // printdata() function is a tool to see what variables "Item" holds
+        public void printdata(){
+            System.out.println("ID:" + this.id + " | COST: " + this.cost + " | DESCRIPTION: " + this.TSetDescr);
+
         }
     }
 
+    // Intialize TreeMap and HashMap to null
+    TreeMap<Integer, Item> TrMap = null;
+    HashMap<Item, TreeSet<Integer>> HaMap = null;
 
-    //TreeMap<Integer, Entry> TreeMap = null;
-    //HashMap<Integer, TreeSet<Integer>> hash_map = null;
-
-
-
-
-    // Add fields of MDS here
-    // Constructors
+    // Constructors, allocate TreeMap and HashMap
     public MDS() {
-       //hash_map = new HashMap<>(null);
+        TrMap = new TreeMap<>();
+        HaMap = new HashMap<>();
     }
+
 
     /* Public methods of MDS. Do not change their signatures.
        __________________________________________________________________
@@ -58,19 +81,52 @@ public class MDS {
        is null or empty, in which case, just the price is updated. 
        Returns 1 if the item is new, and 0 otherwise.
     */
-    public int insert(int id, int price, java.util.List<Integer> list) {
+    // insert() function can now add in new Objects to TreeMap and HashMap
+    public int insert(int id, int price, List<Integer> list) {
 
-        /*      <TESTING ENTRY>
-        Entry new_entry = new Entry(id, price, list);
-        Iterator<Integer> testing = new_entry.TSet.iterator();   
-        while(testing.hasNext()){
-            Integer element = testing.next();
-            System.out.println(element);  
+        // Intialize a int varibale to see if insert succesfully added new Item Hash and Tree Maps
+        int insert_success = 0;
+
+        // Get Item or null from TreeMap        
+        Item get_item = TrMap.get(id);
+
+        // If "get_item" is null in hash, allocate new Item and put it in "TMap"
+        if(get_item == null){
+
+            // Allocate Item object and pass in varibles
+            Item new_item = new Item(id, price, list);
+
+            // For TrMap, we put in id for key and Item object for value
+            TrMap.put(id, new_item);
+            // For HaMap, put in Item object for key and BinaryTree from "Item" for value
+            HaMap.put(new_item, new_item.TSetDescr);
+
+            // Set "insert_success" to 1
+            insert_success = 1;    
         }
-         */
-        
-        return 0;
+
+        // Go here if ID exist in the Hash and Tree Map        
+        // NOTE: If we are here, then we are either only setting new price or descriptor 
+        else{
+
+            // If list is Empty or null, then we only need to change price
+            if(list == null || list.isEmpty()){
+                get_item.setprice(price);
+            }
+
+            // If list is not empty, then get rid of old descriptor and setting new one
+            else{
+
+                get_item.setprice(price);
+                get_item.setlist(list);
+
+                // Assign new Binary Tree to Hash Map
+                HaMap.put(get_item, get_item.TSetDescr);
+            }
+        }
+        return insert_success;
     }
+
 
     // b. Find(id): return price of item with given id (or 0, if not found).
     public int find(int id) {
@@ -123,5 +179,23 @@ public class MDS {
     public int removeNames(int id, java.util.List<Integer> list) {
 	return 0;
     }
+
+
+    // Helper function print_result() to help us see Keys and Values for Tree and Hash Maps
+    public void print_result(){
+        for(int item : TrMap.keySet()){
+        
+            System.out.println("TreeMap ID: " + item);
+            TrMap.get(item).printdata();
+
+            System.out.println(HaMap.get(TrMap.get(item)));
+
+            if(HaMap.get(TrMap.get(item)) == TrMap.get(item).TSetDescr){
+                System.out.println("BOTH TreeMap.Item.BSTree and HashMap.Value are Equal...\n");
+            }
+        }
+    }
+
+
 }
 
